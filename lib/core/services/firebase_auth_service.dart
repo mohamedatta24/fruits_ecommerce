d8bd1e13c0276c714ef9757e37a08de1ad5968cd
Mohamed Atta <mohamedatta24@gmail.com>
2026-04-03 07:43:05 +0200

    finsh failure and custom Exception and firebase auth create using email and password

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_ecommerce/core/errors/custom_exception.dart';
+
+class FirebaseAuthService {
+  Future<User> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException('The account already exists for that email.');
+      } else {
+        throw CustomException('An unexpected error occurred');
+      }
+    } catch (e) {
+      throw CustomException('An unexpected error occurred');
+    }
+  }
+}
